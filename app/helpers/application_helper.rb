d1a7b60bb6a3402acaa23c65e0c381f896a965c5
Ryan Nielson <ryan.nielson@gmail.com>
2013-04-06 22:08:49 -0300

    Editing helpers to clean up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,35 +5,15 @@ module ApplicationHelper
 
   def tag_link_to(tag, count)
     current_tags = get_current_tags(tag)
+    
     link_to raw("#{tag} (#{count}) <span class='light'>&nbsp;</span>"), params.merge({:"tags" => current_tags.any? ? current_tags : ['none']}), class: (current_tags.include?(tag) ? '' : 'active')   
   end
 
   def type_link_to(type, type_plural)
     type = type.to_s
-    type_plural = type_plural.to_s
-
-    #require 'pp'
     current_types = get_current_types(type)
 
-    link_to raw("<span class='entypo #{type_plural}'>&nbsp;</span>"), params.merge({:"types" => current_types.any? ? current_types : ['none']}), class: (current_types.include?(type) ? '' : 'active')
-
-    # current_types = params[:types].try(:dup) || nil
-    # all_types = %w{photo video text misc}
-
-    # if current_types.nil?
-    #   current_types = all_types.reject { |t| t == type } 
-    #   link_to raw("<span class='entypo #{type_plural}'>&nbsp;</span>"), params.merge({:"types" => current_types}), class: 'active'
-    # else
-    #   if current_types.include? type
-    #     currently_active = true
-    #     current_types = current_types.reject { |t| t == type } 
-    #   else
-    #     current_types = (current_types << type)
-    #   end
-
-    #   current_types = current_types.reject { |t| t == 'none' } 
-    #   link_to raw("<span class='entypo #{type_plural}'>&nbsp;</span>"), params.merge({:"types" => current_types.any? ? current_types : ['none']}), class: (currently_active ? 'active' : '')
-    # end
+    link_to raw("<span class='entypo #{type_plural.to_s}'>&nbsp;</span>"), params.merge({:"types" => current_types.any? ? current_types : ['none']}), class: (current_types.include?(type) ? '' : 'active')
   end
 
   def get_current_tags(tag)

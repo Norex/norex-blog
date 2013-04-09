71fbee219dd847d9e00ee9c41eddf2e7d88013f1
Ryan Nielson <ryan.nielson@gmail.com>
2013-04-09 10:44:34 -0300

    Began pagination

--- app/controllers/tumbles_controller.rb
+++ app/controllers/tumbles_controller.rb
@@ -1,6 +1,6 @@
 class TumblesController < ApplicationController
   def index
-    @tumbles = Tumble.get_by_types(params).get_by_tags(params).get_by_users(params).order('date DESC')
+    @tumbles = Tumble.get_by_types(params).get_by_tags(params).get_by_users(params).order('date DESC').page(params['page'] || 1).per(1)
 
     respond_to do |format|
       format.html
@@ -19,18 +19,6 @@ class TumblesController < ApplicationController
       #@tumbles = Tumble.where('title LIKE ?', "%#{@query}%").get_by_types(params).get_by_tags(params).order('date DESC')
       @tumbles = Tumble.where('title LIKE ?', "%#{@query}%").order('date DESC')
     end
-
-    # require 'pp'
-    # tag_list = []
-    
-    # @tag_list = @tumbles.map { |t| t.tags }.flatten.uniq
-    # @type_list = @tumbles.map { |t| t.content_type }.uniq
-    
-    # @tag_list.each do |t| 
-    #   puts t.count
-    # end
-    # pp @tag_list
-    # pp @type_list
 
     respond_to do |format|
       format.html
